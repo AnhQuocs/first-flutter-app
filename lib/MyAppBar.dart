76882daa4cfd06160d7602a8d61886d239b7658d
AnhQuocs <anhquocydkl@gmail.com>
2025-08-21 17:53:05 +0700

    basic scaffold

---
+++ lib/MyAppBar.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class MyAppBar extends StatelessWidget {
+  const MyAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("First App"),
+        backgroundColor: Colors.white,
+        // elevation: 16,
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.search)
+          )
+        ],
+      ),
+
+      backgroundColor: Color(0xFFEEEEEE),
+
+      body: Center(
+        child: Text("Content"),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+          onPressed: (){},
+          child: const Icon(Icons.add),
+      ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+          BottomNavigationBarItem(icon: Icon(Icons.history), label: "History"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/MyScaffold.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class MyScaffold extends StatelessWidget {
+  const MyScaffold({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("First App"),
+        backgroundColor: Colors.white
+      ),
+
+      backgroundColor: Color(0xFFEEEEEE),
+
+      body: Center(
+        child: Text("Content"),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            print("pressed");
+          },
+          child: const Icon(Icons.add),
+      ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+          BottomNavigationBarItem(icon: Icon(Icons.history), label: "History"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+        ],
+      ),
+    );
+  }
+}
